3851c6568401141892b68f6341feed7ee6282db4
Ethan Henderson <ethan.henderson397@gmail.com>
2020-08-17 02:03:27 -0400

    got the load more button to work

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let main = document.querySelector("#main")
 let sections = []
 
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 function makeSections() {
     let newDiv = document.createElement('section')
     main.appendChild(newDiv)
@@ -14,4 +19,12 @@ for (var i = 0; i < 20; i++) {
 
 function sectionDelay(i) {
   setTimeout(makeSections, 250*i)
+}
+
+function loadMore() {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    for (var i = 0; i < 20; i++) {
+      sectionDelay(i)
+    }
+  }
 }
