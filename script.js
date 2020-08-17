5afbec3bbf2dbfc0d44db633ec5a7dc4aed94a15
Ethan Henderson <ethan.henderson397@gmail.com>
2020-08-17 02:25:23 -0400

    finshed initial web layout, will refactor js when my brain works

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 let main = document.querySelector("#main")
 let sections = []
+let loadBtn = document.querySelector('#loadMore')
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
 function makeSections() {
     let newDiv = document.createElement('section')
     main.appendChild(newDiv)
     newDiv.classList.add('astroBox')
     sections.push(newDiv)
 }
-
-for (var i = 0; i < 20; i++) {
-  sectionDelay(i)
+function loading() {
+  loadBtn.style.display = "none"
+  for (var i = 0; i < 20; i++) {
+    sectionDelay(i)
+  }
+  setTimeout(()=>{loadBtn.style.display='block'},4750)
 }
+loading()
 
 function sectionDelay(i) {
   setTimeout(makeSections, 250*i)
-}
-
-function loadMore() {
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    for (var i = 0; i < 20; i++) {
-      sectionDelay(i)
-    }
-  }
 }
